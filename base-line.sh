8ece0478ed5bf07d99a3314bd7c34e3fe3218b42
Jefferson Carley <jeff.carley@gmail.com>
2012-05-15 07:21:43 -0500

    Comments out user administration.

--- base-line.sh
+++ base-line.sh
@@ -10,12 +10,12 @@ if [ ! $(which add-apt-repository) ]; then
   sudo apt-get install -y python-software-properties
 fi
 
-if [ ! $(cat /etc/group | cut -d: -f1 | grep "^admin$") ]; then
-  sudo groupadd admin
-fi
+#if [ ! $(cat /etc/group | cut -d: -f1 | grep "^admin$") ]; then
+  #sudo groupadd admin
+#fi
 
-if [ ! $( groups | grep "\sadmin\s") ]; then
-  sudo useradd -G admin deployer
-fi
+#if [ ! $( groups | grep "\sadmin\s") ]; then
+  #sudo useradd -G admin deployer
+#fi
 
 
