847726d0229d82a30f33c5f92b4b7eb9d474603d
Jefferson Carley <jeff.carley@gmail.com>
2012-05-15 07:20:10 -0500

    Initial commit

---
+++ base-line.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+
+if [ ! $(which git) ]; then
+  sudo apt-get install -y git-core
+fi
+
+if [ ! $(which add-apt-repository) ]; then
+  sudo apt-get install -y python-software-properties
+fi
+
+if [ ! $(cat /etc/group | cut -d: -f1 | grep "^admin$") ]; then
+  sudo groupadd admin
+fi
+
+if [ ! $( groups | grep "\sadmin\s") ]; then
+  sudo useradd -G admin deployer
+fi
+
+
