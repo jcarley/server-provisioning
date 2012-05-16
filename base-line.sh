28cb24fa2849d0dd47fd15f289c393bdc9adef53
Jefferson Carley <jeff.carley@gmail.com>
2012-05-16 18:21:24 -0500

    adds rbenv to the load path.

--- base-line.sh
+++ base-line.sh
@@ -30,13 +30,45 @@ if [ $(id -u) -eq 0 ]; then
     sudo passwd deployer
   fi
 
-  #if [ ! $( groups | grep "\sadmin\s") ]; then
-    #sudo useradd -G admin deployer
-  #fi
+  # change to deployer user
+  su deployer
+
+  RBENV_ROOT="$HOME/.rbenv"
+
+  if [ ! -d "$RBENV_ROOT" ] ; then
+    # check for, and install rbenv
+    #curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+
+    # take a backup of .bashrc
+    cp .bashrc .bashrc.original
+
+    # insert lines at top of .bashrc
+    echo "if [ -d \$HOME/.rbenv ]; then" >> bashrc.tmp
+    echo "  export PATH=\"\$HOME/.rbenv/bin:\$PATH\"" >> bashrc.tmp
+    echo "  eval \"\$(rbenv init -)\"" >> bashrc.tmp
+    echo "fi" >> bashrc.tmp
+    cat .bashrc >> .bashrc.tmp
+    mv .bashrc.tmp .bashrc
+
+    # source .bashrc
+    #source ~/.bashrc
+  
+    # install some server essentials
+    #rbenv bootstrap-ubuntu-11-10
+
+    # install ruby
+    #rbenv install 1.9.3-p125
+
+    # set 1.9.3 as our global ruby
+    #rbenv global 1.9.3-p125
+
+    # install gems
+    #rbenv bootstrap
+
+    # install bundler
+    #gem install bundler --no-ri --no-rdoc
+  fi
 
 fi
-
-
-
 
 
