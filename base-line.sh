13cfa43d83d4f61f66cd439c9261b9e411c9b347
Jefferson Carley <jeff.carley@gmail.com>
2012-05-16 18:29:08 -0500

    Adds progress messages.

--- base-line.sh
+++ base-line.sh
@@ -7,20 +7,28 @@ if [ $(id -u) -eq 0 ]; then
 
   if [ ! $(which git) ]; then
     sudo apt-get install -y git-core
+  else
+    echo "Git already installed."
   fi
 
   if [ ! $(which add-apt-repository) ]; then
     sudo apt-get install -y python-software-properties
+  else
+    echo "Python software properties already installed."
   fi
   
   if [ ! $(cat /etc/group | cut -d: -f1 | grep "^admin$") ]; then
     sudo groupadd admin
+  else
+    echo "Group admin exists."
   fi
 
   egrep "^puppet" /etc/passwd > /dev/null
   if [ $? -ne 0 ]; then
     # user does not exist, so add
     sudo useradd --comment "Puppet" --no-create-home --system --shell /bin/false puppet
+  else
+    echo "User puppet exists."
   fi
 
   egrep "^deployer" /etc/passwd > /dev/null
@@ -28,21 +36,28 @@ if [ $(id -u) -eq 0 ]; then
     # user does not exist, so add
     sudo useradd -m --comment "Deployer" --shell /bin/bash --groups admin deployer
     sudo passwd deployer
+  else
+    echo "User deployer already exists."
   fi
 
   # change to deployer user
+  echo "Switching to deployer user."
   su deployer
 
   RBENV_ROOT="$HOME/.rbenv"
+
+  echo "Install rbenv at $RBENV_ROOT"
 
   if [ ! -d "$RBENV_ROOT" ] ; then
     # check for, and install rbenv
     #curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
 
     # take a backup of .bashrc
+    echo "Making a backup of the .bashrc"
     cp .bashrc .bashrc.original
 
     # insert lines at top of .bashrc
+    echo "Adding rbenv to the load path"
     echo "if [ -d \$HOME/.rbenv ]; then" >> bashrc.tmp
     echo "  export PATH=\"\$HOME/.rbenv/bin:\$PATH\"" >> bashrc.tmp
     echo "  eval \"\$(rbenv init -)\"" >> bashrc.tmp
