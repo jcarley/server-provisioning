0312fb21b4fd11f6cab397d54b462669a51e4e55
Jefferson Carley <jeff.carley@gmail.com>
2012-05-19 22:36:01 -0500

    syntax changes.

--- solo.rb
+++ solo.rb
@@ -1,4 +1,3 @@
 cookbook_path File.expand_path("../cookbooks", __FILE__)
 json_attribs File.expand_path("../node.json", __FILE__)
 file_cache_path "/var/chef/cache"
-Ohai::Config[:plugin_path] = "/etc/chef/ohai_plugins"
