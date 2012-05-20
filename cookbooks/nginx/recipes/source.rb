1aa0ccec57239b13968967d0c536e912a47221b4
Jefferson Carley <jeff.carley@gmail.com>
2012-05-20 07:17:34 -0500

    Forces nginx to no recompile.

--- cookbooks/nginx/recipes/source.rb
+++ cookbooks/nginx/recipes/source.rb
@@ -89,7 +89,7 @@ bash "compile_nginx_source" do
   EOH
   
   not_if do
-    false
+    true
     #nginx_force_recompile == false &&
       #node.automatic_attrs[:nginx][:version] == node[:nginx][:version] &&
       #node.automatic_attrs[:nginx][:configure_arguments].sort == configure_flags.sort
