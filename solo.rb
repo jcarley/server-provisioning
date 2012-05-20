2bf5774b55684fff80abf64c17eaac94f4c6e60b
Jefferson Carley <jeff.carley@gmail.com>
2012-05-19 22:25:08 -0500

    Adding config settings.

--- solo.rb
+++ solo.rb
@@ -1,2 +1,4 @@
 cookbook_path File.expand_path("../cookbooks", __FILE__)
 json_attribs File.expand_path("../node.json", __FILE__)
+file_cache_path "/var/chef/cache"
+Ohai::Config[:plugin_path] = "/etc/chef/ohai_plugins"
