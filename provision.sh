7073b883f496e200d1d9890f80b5f950e5d91c25
Jefferson Carley <jeff.carley@gmail.com>
2012-08-01 18:40:39 -0500

    Added ability to specifiy an alternate name for a cloned repo.

--- provision.sh
+++ provision.sh
@@ -2,18 +2,18 @@
 
 get_bundle() {
   (
-  if [ -d "$2" ]; then
-    echo "Updating $1's $2"
-    cd "$2"
+  if [ -d "$3" ]; then
+    echo "Updating $1's $3"
+    cd "$3"
     git pull --rebase
   else
-    git clone "git://github.com/$1/$2.git"
+    git clone "git://github.com/$1/$2.git" "$3"
   fi
   )
 }
 
 cd /etc/puppet/modules
-get_bundle puppetlabs puppetlabs-nginx
+get_bundle puppetlabs puppetlabs-nginx nginx
 
 cd /etc/puppet
 puppet apply manifests/site.pp
