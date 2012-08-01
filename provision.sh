303770acc776ed4ad4a1c6bcc255784ad5be42f7
Jefferson Carley <jeff.carley@gmail.com>
2012-08-01 18:19:59 -0500

    Adding provision script to pull puppet recipes from github.

---
+++ provision.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+get_bundle() {
+  (
+  if [ -d "$2" ]; then
+    echo "Updating $1's $2"
+    cd "$2"
+    git pull --rebase
+  else
+    git clone "git://github.com/$1/$2.git"
+  fi
+  )
+}
+
+cd /etc/puppet/modules
+get_bundle puppetlabs puppetlabs-nginx
+
+puppet apply manifests/site.pp
