075f1c2ecffeed399b9dea11826a5e6b1358e432
Jefferson Carley <jeff.carley@gmail.com>
2012-08-01 20:32:21 -0500

    Added my own nginx module.

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,15 +2,6 @@ Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 }
 
-package {
-  "nginx" :
-    ensure => present
-}
-
-service {
-  "nginx":
-    ensure => true,
-    enable => true
-}
+include nginx
 
 
---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,42 @@
+class nginx {
+  package { "nginx": ensure => installed }
+
+  service { "nginx":
+    enable => true,
+    ensure => running,
+  }
+
+  exec { "reload nginx":
+    command => "/usr/sbin/service nginx reload",
+    require => Package["nginx"],
+    refreshonly => true,
+  }
+
+  file { "/etc/nginx/nginx.conf":
+    source => "puppet:///modules/nginx/nginx.conf",
+    notify => Exec["reload nginx"],
+    require => Package["nginx"],
+  }
+}
+
+define site( $sitedomain = "" ) {
+  include nginx
+
+  if $sitedomain == "" {
+    $vhost_domain = $name
+  } else {
+    $vhost_domain = $sitedomain
+  }
+
+  file { "/etc/nginx/sites-available/${vhost_domain}.conf":
+    content => template("nginx/vhost.erb"),
+    require => Package["nginx"],
+  }
+
+  file { "/etc/nginx/sites-enabled/${vhost_domain}.conf":
+    ensure => link,
+    target => "/etc/nginx/sites-available/${vhost_domain}.conf",
+    require => File["/etc/nginx/sites-available/${vhost_domain}.conf"],
+    notify => Exec["reload nginx"],
+  }
+}
