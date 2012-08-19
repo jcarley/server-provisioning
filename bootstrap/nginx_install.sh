3fba32292eece3d83ee08f23e4d61ca08a307c56
Jefferson Carley <jeff.carley@gmail.com>
2012-08-19 00:33:29 -0500

    Code clean up.

--- bootstrap/nginx_install.sh
+++ bootstrap/nginx_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CC=gcc-4.4
+
 V=1.2.3
 prefix=/opt/nginx
 etc=/usr/local/etc
@@ -11,8 +13,6 @@ upload_module_path=/tmp/nginx_upload_module-2.2.0
 upload_progress_module_name=nginx_uploadprogress_module-0.9.0.tar.gz
 upload_progress_module_path=/tmp/masterzen-nginx-upload-progress-module-a788dea
 
-# mkdir -p $upload_module_path
-# mkdir -p $upload_progress_module_path
 sudo mkdir -p $etc/nginx
 sudo mkdir -p $var/nginx
 sudo mkdir -p $var/run
@@ -26,21 +26,17 @@ fi
 
 if [ ! -d $upload_module_path ]; then
   cd /tmp
-  # wget http://www.grid.net.ru/nginx/download/nginx_upload_module-2.2.0.tar.gz
   wget http://www.grid.net.ru/nginx/download/$upload_module_name
   tar -xzvf $upload_module_name
 fi
 
 if [ ! -d $upload_progress_module_path ]; then
   cd /tmp
-  # wget https://github.com/downloads/masterzen/nginx-upload-progress-module/nginx_uploadprogress_module-0.9.0.tar.gz
   wget https://github.com/downloads/masterzen/nginx-upload-progress-module/$upload_progress_module_name
   tar -xzvf $upload_progress_module_name
 fi
 
 cd /tmp/nginx-$V
-
-export CC=gcc-4.4
 
 ./configure --prefix=$prefix --with-http_ssl_module --with-pcre --with-ipv6 --conf-path=$etc/nginx/nginx.conf --pid-path=$var/run/nginx.pid --lock-path=$var/nginx/nginx.lock --add-module=$upload_module_path --add-module=$upload_progress_module_path
 make
