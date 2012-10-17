1c55eff2bc1904a97d4579dc33dfb9b84fef2f1d
Jefferson Carley <jeff.carley@gmail.com>
2012-10-16 20:15:47 -0500

    Adds manifest to add a puma app, and a template for a puma config.

--- modules/puma/manifests/app.pp
+++ modules/puma/manifests/app.pp
@@ -1,7 +1,16 @@
 class puma::app($app_path = "", $user = "", $config_file_path = "", $log_file_path = "") {
+  $puma_tmp = "${app_path}/tmp/puma"
 
   if $config_file_path == "" {
     $config_file = "${app_path}/config/puma.rb"
+
+    file { $config_file:
+      content => template('puma/puma_config.erb'),
+      ensure  => present,
+      owner   => $user,
+      group   => $user
+    }
+
   } else {
     $config_file = $config_file_path
   }
@@ -12,13 +21,14 @@ class puma::app($app_path = "", $user = "", $config_file_path = "", $log_file_pa
     $log_file = $log_file_path
   }
 
-  file { "${app_path}/tmp/puma":
+  file { ${puma_tmp}:
     ensure => directory,
   }
 
   exec { add_app:
     command => "/etc/init.d/puma add ${app_path} ${user} ${config_file} ${log_file}",
-    require => File["${app_path}/tmp/puma"],
+    require => [ File["${app_path}/tmp/puma"],
+                 File[$config_file] ],
   }
 
   exec { remove_app:
