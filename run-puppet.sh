c20639e0041fda0ed1638d2d98bc0e90f44bec3d
jcarley <jcarley@gettyimages.com>
2013-05-04 10:15:44 -0500

    Adds links options to copy sym links to remote server.

--- run-puppet.sh
+++ run-puppet.sh
@@ -1,1 +1,1 @@
-rsync -rt --verbose --progress --stats --compress --delete --exclude-from=exclude.txt ./ root@198.199.85.133:/etc/puppet
+rsync -rt --verbose --progress --stats --compress --links --delete --exclude-from=exclude.txt ./ root@198.199.85.133:/etc/puppet
