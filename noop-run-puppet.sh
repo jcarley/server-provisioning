a4f017dc49b4fd8af36ddcb089130066ddfc4800
jcarley <jcarley@gettyimages.com>
2013-05-04 10:09:50 -0500

    Adds shell scripts and exclude patterns for rsync manifests to remote server.

---
+++ noop-run-puppet.sh
@@ -0,0 +1,1 @@
+rsync -nrt --verbose --progress --stats --compress --delete --exclude-from=exclude.txt ./ root@198.199.85.133:/etc/puppet
---
+++ run-puppet.sh
@@ -0,0 +1,1 @@
+rsync -rt --verbose --progress --stats --compress --delete --exclude-from=exclude.txt ./ root@198.199.85.133:/etc/puppet
