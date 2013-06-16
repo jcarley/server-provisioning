ad53654cef83ad700244e51676efdaa14abf3542
jcarley <jcarley@gettyimages.com>
2013-06-16 09:15:28 -0500

    Improvements to the roles user module.

--- modules/roles/manifests/user.pp
+++ modules/roles/manifests/user.pp
@@ -1,27 +1,42 @@
-class roles::user($run_as_user, $ssh_pub_key) {
+define roles::user(
+  $run_as_user,
+  $ssh_pub_key   = undef,
+  $password      = undef,
+  $primary_group = undef,
+  $groups        = [],
+) {
 
-  group { "admin":
-    ensure => "present",
-  }
+  group { $groups:
+    ensure => present,
+  } ->
+
+  group { $primary_group:
+    ensure => present,
+  } ->
 
   user { $run_as_user:
+    comment    => $run_as_user,
+    password   => sha1($password),
     ensure     => 'present',
     shell      => '/bin/bash',
-    groups     => ['admin'],
+    gid        => $primary_group,
+    groups     => $groups,
     home       => "/home/${run_as_user}",
+    membership => minimum,
     managehome => true,
+  } ->
+
+  exec { "$run_as_user homedir":
+    command => "/bin/cp -R /etc/skel /home/$run_as_user; /bin/chown -R $run_as_user:$primary_group /home/$run_as_user",
+    creates => "/home/$run_as_user",
+    require => User[$run_as_user],
   }
 
-  file { "/home/${run_as_user}":
-    ensure  => directory,
-    owner   => $run_as_user,
-    group   => $run_as_user,
+  if $ssh_pub_key {
+    ssh::user { $run_as_user:
+      key     => $ssh_pub_key,
+      require => User[$run_as_user],
+    }
   }
 
-  ssh::user { $run_as_user:
-    key => $ssh_pub_key,
-  }
-
-  Group['admin'] -> User["${run_as_user}"] -> File["/home/${run_as_user}"] ->
-  Ssh::User["${run_as_user}"]
 }
