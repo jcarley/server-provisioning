b1eceb0e2df31a3ec68f4f73995930507a991c1e
jcarley <jcarley@gettyimages.com>
2013-06-16 12:02:22 -0500

    Updated to use the debian apt-get repo for install jenkins.

--- modules/jenkins/manifests/repo/debian.pp
+++ modules/jenkins/manifests/repo/debian.pp
@@ -1,19 +1,10 @@
 class jenkins::repo::debian {
-
-  $install_source = 'http://pkg.jenkins-ci.org/debian/binary/jenkins_1.517_all.deb'
-
-  $install_command = $::operatingsystem ? {
-    /(?i:Debian|Ubuntu|Mint)/ => "wget \'${install_source}\' -O /tmp/jenkins.deb ; dpkg -i /tmp/jenkins.deb",
+  apt::source { 'jenkins':
+    location    => 'http://pkg.jenkins-ci.org/debian',
+    release     => 'binary/',
+    repos       => '',
+    key         => 'D50582E6',
+    key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
+    include_src => false,
   }
-
-  package {
-    'daemon':
-      ensure => installed;
-  }
-
-  exec { "install_jenkins_package":
-    command     => $install_command,
-  }
-
-  Package['daemon'] -> Exec['install_jenkins_package']
 }
