c3d69e179b40c1f44f9d038c0fd1264aa1fecc46
jcarley <jcarley@gettyimages.com>
2013-06-16 23:05:36 -0500

    Adds allow 8080 firewall rule to connect to a vagrant running jenkins ci server.

--- modules/roles/manifests/infrastructure.pp
+++ modules/roles/manifests/infrastructure.pp
@@ -1,5 +1,15 @@
-class roles::infrastructure {
+class roles::infrastructure(
+  $allow_ports = []
+){
   include ufw
+
+  # notice($allow_ports)
+
+  # $allow_ports.foreach { |$port|
+    # ufw::allow { "allow-$port-from-all":
+      # port => $port
+    # }
+  # }
 
   ufw::allow { "allow-ssh-from-all":
     port    => 22,
@@ -8,4 +18,9 @@ class roles::infrastructure {
   ufw::allow { "allow-http-from-all":
     port => 80,
   }
+
+  ufw::allow { "allow-8080-from-all":
+    port => 8080,
+  }
+
 }
